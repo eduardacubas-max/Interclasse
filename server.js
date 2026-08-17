37e1535b73647b9d7ebb435ee81af9f0391c593e
Eduarda <eduarda.cubas@edu.senai.br>
2026-08-17 16:06:54 -0300

    Atualiza classe Turma e atleta

--- server.js
+++ server.js
@@ -1,4 +1,25 @@
 class Turma {
+    constructor(nome) {
+        this.id = Turma.proximoId++;
+        this.nome = nome;
+    }
+
+    adicionar() {
+        console.log("Turma adicionada:", this);
+    }
+
+    listar() {
+        console.log("Turma:", this);
+    }
+
+    remover() {
+        console.log("Turma removida:", this.id);
+    }
+}
+
+Turma.proximoId = 1;
+
+class Atleta {
     constructor(nomeAtleta, idadeAtleta) {
         this.nomeAtleta = nomeAtleta;
         this.idadeAtleta = idadeAtleta;
@@ -11,11 +32,30 @@ class Turma {
 
 let turmas = [];
 
-let turma1 = new Turma("Cypher", 19);
+let turma1 = new Turma("Turma A");
+let turma2 = new Turma("Turma B");
+let turma3 = new Turma("Turma C");
 
 turmas.push(turma1);
+turmas.push(turma2);
+turmas.push(turma3);
 
-turma1.registrar();
+console.log("Turmas cadastradas:");
 
-console.log("Turmas na arena:");
-console.log(turmas);
+for (let i = 0; i < turmas.length; i++) {
+    turmas[i].listar();
+}
+
+turmas = turmas.filter(function(turma) {
+    return turma.id !== 2;
+});
+
+console.log("Depois de remover a turma 2:");
+
+for (let i = 0; i < turmas.length; i++) {
+    turmas[i].listar();
+}
+
+let atleta1 = new Atleta("Cypher", 19);
+
+atleta1.registrar();
